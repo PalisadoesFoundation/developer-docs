e9a6b4bc29ee6d825824f54a8b02307b905c8c0c
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2025-10-12 16:07:30 -0500

    Formatted files with prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
-import HomepageHeader from "../components/HomepageHeader/HomepageHeader";
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageHeader from '../components/HomepageHeader/HomepageHeader';
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
